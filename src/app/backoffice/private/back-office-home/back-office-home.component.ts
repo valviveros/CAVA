14a557954c8f566a9d9ed5a425b50509cc2667af
valviveros <blkfthrs@gmail.com>
2021-05-11 23:12:57 -0500

    Now in sellers area the name of the selles is being brought from the database.

--- src/app/backoffice/private/back-office-home/back-office-home.component.ts
+++ src/app/backoffice/private/back-office-home/back-office-home.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { UserI } from 'src/app/shared/interfaces/UserI';
+import { RegisterService } from "src/app/shared/services/register.service";
 
 @Component({
   selector: 'app-back-office-home',
@@ -8,12 +12,52 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./back-office-home.component.scss']
 })
 export class BackOfficeHomeComponent implements OnInit {
-  @Input() sellersName: string = "Valentina";
+  @Input() sellersName: string = "";
+  registerList: UserI[] = [];
   countMore: number = 0;
+  user: any;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private firebaseAuth: AngularFireAuth, private firebase: AngularFireDatabase, private registerService: RegisterService) { }
 
   ngOnInit(): void {
+    this.registerService.getRegister()
+      .snapshotChanges().subscribe(item => {
+        this.registerList = [];
+        item.forEach(element => {
+          let x = element.payload.toJSON();
+          this.registerList.push(x as UserI);
+        });
+      });
+    this.loadSellersName();
+  }
+
+  async loadSellersName() {
+    let Key;
+    let Email: any;
+    this.firebaseAuth.user.subscribe((data => {
+      this.user = data;
+      Email = this.user['email'];
+    }));
+    await this.firebase.database.ref("users").once("value", (users) => {
+      users.forEach((user) => {
+        const childKey = user.key;
+        const childData = user.val();
+        if (childData.email == Email) {
+          Key = childKey;
+          // console.log("entramos", childKey);
+          // console.log("recorrido", childKey);
+          user.forEach((name => {
+            const nameChildKey = name.key;
+            const nameChildData = name.val();
+            // console.log("entramos", nameChildKey);
+            // console.log("recorrido", nameChildData);
+            if (nameChildKey == 'name') {
+              this.sellersName = nameChildData;
+            }
+          }))
+        }
+      });
+    });
   }
 
   dropDownOptions() {
@@ -33,7 +77,7 @@ export class BackOfficeHomeComponent implements OnInit {
 
   async doLogout() {
     await this.authService.logout();
-    this.router.navigate(['/']);
+    this.router.navigate(['/login']);
   }
 
 }
