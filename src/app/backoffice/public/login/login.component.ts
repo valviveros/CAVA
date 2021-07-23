f9cb1659b5f8ac9f7c56e0ff0d9b1c5d59a1ef98
valviveros <blkfthrs@gmail.com>
2021-07-23 17:56:19 -0500

    Fixed login and register form validations.

--- src/app/backoffice/public/login/login.component.ts
+++ src/app/backoffice/public/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators, FormBuilder } from "@angular/forms";
+import { FormControl, FormGroup, NgForm, Validators, FormBuilder } from '@angular/forms';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Router } from '@angular/router';
-import { RegisterService } from "src/app/shared/services/register.service";
+import { RegisterService } from 'src/app/shared/services/register.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { UserI } from 'src/app/shared/interfaces/UserI';
 
@@ -14,12 +14,11 @@ import { UserI } from 'src/app/shared/interfaces/UserI';
 export class LoginComponent implements OnInit {
   active: number = 5;
   registerList: UserI[] = [];
-  signupForm = new FormGroup({
-    signupEmail: new FormControl(),
-    signupPassword: new FormControl(),
-  });
+  loginForm: FormGroup;
 
-  constructor(private router: Router, private firebase: AngularFireDatabase, private firebaseAuth: AngularFireAuth, private registerService: RegisterService) { }
+  constructor(private router: Router, private firebase: AngularFireDatabase, private firebaseAuth: AngularFireAuth, private registerService: RegisterService, private formBuilder: FormBuilder) {
+    this.loginForm = this.createloginForm();
+   }
 
   ngOnInit(): void {
     this.registerService.getRegister()
@@ -32,9 +31,29 @@ export class LoginComponent implements OnInit {
       });
   }
 
+  createloginForm(): FormGroup {
+    return this.formBuilder.group(
+      {
+        signupEmail: [
+          null,
+          Validators.compose([
+            Validators.email,
+            Validators.required
+          ])
+        ],
+        signupPassword: [
+          null,
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(6)
+          ])
+        ]
+      })
+  }
+
   doLogin() {
-    let email = this.signupForm.controls.signupEmail.value;
-    const password = this.signupForm.controls.signupPassword.value;
+    let email = this.loginForm.controls.signupEmail.value;
+    const password = this.loginForm.controls.signupPassword.value;
 
     let emailRegexp = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
 
@@ -54,22 +73,27 @@ export class LoginComponent implements OnInit {
         // console.log(`Error [${errorCode}]: ${errorMessage}`);
         const query: string = '.loginContainer #passwordWrong';
         const passwordWrong: any = document.querySelector(query);
-        passwordWrong.style.display = "flex";
+        passwordWrong.style.display = 'flex';
         setTimeout(() => {
-          passwordWrong.style.display = "none";
+          passwordWrong.style.display = 'none';
         }, 3000);
       });
     } else {
       const query: string = '.loginContainer #userDoesNotExist';
       const userDoesNotExist: any = document.querySelector(query);
-      userDoesNotExist.style.display = "flex";
+      userDoesNotExist.style.display = 'flex';
       setTimeout(() => {
-        userDoesNotExist.style.display = "none";
+        userDoesNotExist.style.display = 'none';
       }, 3000);
     }
   }
 
   goToRegister() {
-    this.router.navigate(["/register"]);
+    this.router.navigate(['/register']);
+  }
+
+  validateField(controlName: string): boolean {
+    let control = this.loginForm.controls[controlName]
+    return control.invalid && control.touched
   }
 }
