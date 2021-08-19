78b91ef8b495fa6c5f7adb1d8b89c6592e00f608
valviveros <blkfthrs@gmail.com>
2021-08-19 15:17:34 -0500

    Fixed heights for some components.

--- src/app/shared/components/company-card/company-card.component.ts
+++ src/app/shared/components/company-card/company-card.component.ts
@@ -6,10 +6,10 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./company-card.component.scss']
 })
 export class CompanyCardComponent implements OnInit {
-  @Input() shopId: string = ''!;
-  @Input() companyLogo: string = ''!;
-  @Input() companyName: string = ''!;
-  @Input() companyPhoto: string = ''!;
+  @Input() shopId: any;
+  @Input() companyLogo: any;
+  @Input() companyName: any;
+  @Input() companyPhoto: any;
 
   constructor() { }
 
